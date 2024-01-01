d6508048fcfa81e0b02aa37f436fe4ad54225c5d
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2024-01-01 17:18:38 -0300

    [main] Ajustando método de formatação de número

--- calcule/src/main/java/com/example/calcule/CalculeApplication.java
+++ calcule/src/main/java/com/example/calcule/CalculeApplication.java
@@ -1,13 +1,17 @@
 package com.example.calcule;
 
+import com.example.calcule.service.CalculadoraService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CalculeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculeApplication.class, args);
+		CalculadoraService calculadoraService = new CalculadoraService();
+		System.out.println(calculadoraService.subtracao(4d, 5d));
 	}
 
 }
--- calcule/src/main/java/com/example/calcule/service/CalculadoraService.java
+++ calcule/src/main/java/com/example/calcule/service/CalculadoraService.java
@@ -1,36 +1,38 @@
 package com.example.calcule.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculadoraService
 {
-    Double operadorUm;
-    Double operadorDois;
 
-    public Double soma(){
+    public Double soma(Double operadorUm, Double operadorDois){
         Double soma = operadorUm + operadorDois;
         numeroFormatado(soma);
         return soma;
     }
 
-    public Double subtracao(){
+    public Double subtracao(Double operadorUm, Double operadorDois){
         Double subtracao = operadorUm - operadorDois;
         numeroFormatado(subtracao);
         return subtracao;
     }
 
-    public Double multiplicacao(){
+    public Double multiplicacao(Double operadorUm, Double operadorDois){
         Double multiplicacao = operadorUm * operadorDois;
         numeroFormatado(multiplicacao);
         return multiplicacao;
     }
 
-    public Double divisao(){
+    public Double divisao(Double operadorUm, Double operadorDois){
         Double divisao = operadorUm/operadorDois;
         numeroFormatado(divisao);
         return divisao;
     }
 
     public Double numeroFormatado(Double numeroNaoFormatado){
-        String numeroFormatado = String.format("%2f", numeroNaoFormatado);
-        return Double.valueOf(numeroFormatado);
+        String numeroFormatadoString = String.format("%.2f", numeroNaoFormatado);
+        String numeroFormatado = numeroFormatadoString.replace(",", ".");
+        return Double.parseDouble(numeroFormatado);
     }
 }
